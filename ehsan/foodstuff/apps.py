2b0c1f195fdf1c993b860f192a050bea3911a618
mohammad mashayekhi <mamashayekhi18@gmail.com>
2024-05-30 19:11:57 +0330

    start project

---
+++ ehsan/foodstuff/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class FoodstuffConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'foodstuff'
---
+++ ehsan/foodstuff/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from shortuuid.django_fields import ShortUUIDField
+
+
+
+SCALE = {
+    ("Weight" , "وزن(کیلوگرم)"),
+    ("Number" , "(عدد)تعداد"),
+}
+
+class Category(models.Model):
+    cat_id = ShortUUIDField(unique=True , length=10 , max_length=30 , alphabet="abcdefgh12345")
+    cat_name = models.CharField(max_length = 300)
+
+    def __str__(self):
+         return self.cat_name
+
+
+class Stuffs(models.Model):
+    stuff_id = ShortUUIDField(unique=True , length=10 , max_length=30 , alphabet="abcdefgh12345")
+    stuff_name = models.CharField(max_length = 300)
+    stuff_category = models.ManyToManyField(Category , related_name='stuff_category')
+    stuff_scale = models.CharField(choices=SCALE , max_length=20 , default="Weight")
+
+    def __str__(self):
+         return self.part_name_fa
