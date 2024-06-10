13be9308d6f7cfad73fe81d048d77a59580905bf
mohammad mashayekhi <mamashayekhi18@gmail.com>
2024-06-10 11:19:33 +0330

    commit

--- ehsan/repository/views.py
+++ ehsan/repository/views.py
@@ -7,27 +7,18 @@ from foodstuff.models import Stuffs,Category
 def repository(request):
     return render(request, 'repository/repository.html')
 
+
 def in_repository(request, date):
     gregorian_date = datetime.strptime(date, '%Y-%m-%d').date()  # تبدیل تاریخ به فرمت میلادی
     initial_type = 'in'
 
     repository_instance = None
     initial_data = None  # تعیین مقدار اولیه برای initial_data
-    all_stuff_ids = Stuffs.objects.values_list('id', flat=True)
+    all_stuff_ids = Stuffs.objects.values_list('stuff_id', flat=True)
 
     if Repository.objects.filter(date=gregorian_date, type='in').exists():  # بررسی وجود رکورد
             repository_instance = Repository.objects.get(date=gregorian_date, type='in')
             initial_data = {'type': initial_type, **{'stuff_' + str(stuff_id): quantity for stuff_id, quantity in repository_instance.quantities.items()}}
-            
-    if initial_data is None:
-        initial_data = {
-            'type': initial_type,
-            **{'stuff_' + str(stuff_id): 0 for stuff_id in all_stuff_ids}
-        }        
-   
-    for key, value in initial_data.items():
-        if value is None:
-            initial_data[key] = 0
             
     if request.method == 'POST':
         form = RepositoryForm(request.POST, initial=initial_data)
@@ -46,6 +37,10 @@ def in_repository(request, date):
         initial_form_data = {'date': gregorian_date, 'type': initial_type}
         if initial_data:
             initial_form_data.update(initial_data)  # فقط اگر initial_data موجود باشد، آن را به initial_form_data اضافه کنید
+        else:
+            initial_data = {'type': initial_type,**{'stuff_' + str(stuff_id): 0 for stuff_id in all_stuff_ids}}
+            initial_form_data.update(initial_data)  # فقط اگر initial_data موجود باشد، آن را به initial_form_data اضافه کنید
+        print(initial_form_data)
         form = RepositoryForm(initial=initial_form_data)  # استفاده از یک دیکشنری برای ارسال به عنوان initial
     
     categories = Category.objects.all()  # اضافه کردن دسته بندی‌ها
