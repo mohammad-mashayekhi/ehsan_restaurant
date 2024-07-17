8fc0deb16d33694e53d855334c7853e31345357e
mohammad mashayekhi <mamashayekhi18@gmail.com>
2024-07-17 19:30:52 +0330

    add automasion in dashboard template

--- ehsan/ehsan/views.py
+++ ehsan/ehsan/views.py
@@ -3,11 +3,8 @@ from django.contrib.auth import authenticate, login,logout
 # from .forms import LoginForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import AuthenticationForm
-
-@login_required
-def dashboard(request):
-    return render(request, 'main/dashboard.html')
-
+import jdatetime
+from datetime import datetime
 
 def login_view(request):
     if request.method == 'POST':
@@ -31,4 +28,62 @@ def logout_view(request):
     logout(request)
     return redirect('dashboard')  # به صفحه اصلی یا هر صفحه دیگری که می‌خواهید کاربر بعد از خروج به آن هدایت شود.
 
+from django.shortcuts import render
+from repository.models import Repository
+from foodstuff.models import Price
+from recipe.models import RecipeSaleFile
 
+
+
+@login_required
+def dashboard(request):
+    import jdatetime
+    from datetime import datetime
+
+    today_date = datetime.now().strftime('%Y-%m-%d')
+    gregorian_date = datetime.strptime(today_date, '%Y-%m-%d')
+    jalali_date = jdatetime.date.fromgregorian(date=gregorian_date).strftime('%Y/%m/%d')
+    
+    def convert_to_jalali(gregorian_date):
+        if gregorian_date:
+            return jdatetime.date.fromgregorian(date=gregorian_date).strftime('%Y/%m/%d')
+        return None
+    
+    def is_today(gregorian_date):
+        if gregorian_date:
+            return gregorian_date == datetime.now().date()
+        return False
+
+    # دریافت آخرین ورودی انبار
+    last_entry_in = Repository.objects.filter(type='in').order_by('-date').first()
+    last_entry_in_date = convert_to_jalali(last_entry_in.date) if last_entry_in else None
+    last_entry_in_today = is_today(last_entry_in.date) if last_entry_in else False
+
+    # دریافت آخرین خروجی انبار
+    last_entry_out = Repository.objects.filter(type='out').order_by('-date').first()
+    last_entry_out_date = convert_to_jalali(last_entry_out.date) if last_entry_out else None
+    last_entry_out_today = is_today(last_entry_out.date) if last_entry_out else False
+
+    # دریافت آخرین اطلاعات فروش
+    last_sale_info = RecipeSaleFile.objects.order_by('-date_created').first()
+    last_sale_info_date = convert_to_jalali(last_sale_info.date_created) if last_sale_info else None
+    last_sale_info_today = is_today(last_sale_info.date_created) if last_sale_info else False
+
+    # دریافت آخرین زمان آپدیت قیمت مواد اولیه
+    last_price_update = Price.objects.order_by('-date').first()
+    last_price_update_date = convert_to_jalali(last_price_update.date) if last_price_update else None
+    last_price_update_today = is_today(last_price_update.date) if last_price_update else False
+
+    context = {
+        'today_date':today_date,
+        'jalali_date': jalali_date,
+        'last_entry_in_date': last_entry_in_date,
+        'last_entry_in_today': last_entry_in_today,
+        'last_entry_out_date': last_entry_out_date,
+        'last_entry_out_today': last_entry_out_today,
+        'last_sale_info_date': last_sale_info_date,
+        'last_sale_info_today': last_sale_info_today,
+        'last_price_update_date': last_price_update_date,
+        'last_price_update_today': last_price_update_today,
+    }
+    return render(request, 'main/dashboard.html', context)
